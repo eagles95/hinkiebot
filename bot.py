2a41c2752ab22d8c0cb98db7eb3a80e09ca943cf
Joao Dias <jfcdias@sga.pucminas.br>
2018-03-13 20:12:38 -0300

    added more rooms

--- bot.py
+++ bot.py
@@ -14,23 +14,24 @@ class HinkieBot(ch.RoomManager):
         print("Disconnected from "+room.name)
 
     def onMessage(self, room, user, message):
-        msg = str(message.body).lstrip()
-        print(msg)
-        if (msg[0] == prefix):
-            msg = msg[1:]
-            try:
-                command,args = msg.split(" ",1)
-                print("command" + command)
-                print("args" + args)
-                ret = commands.runCommand(command,args)
-            except:
-                ret = commands.runCommand(msg,None)
-            if(ret!=None):
-                room.message(str(ret))
+        try:
+            msg = message.body.encode("utf-8").lstrip()
+            print(msg)
+            if (msg[0] == prefix):
+                msg = msg[1:]
+                try:
+                    command,args = msg.split(" ",1)
+                    print("command" + command)
+                    print("args" + args)
+                    ret = commands.runCommand(command,args)
+                except:
+                    ret = commands.runCommand(msg,None)
+                if(ret!=None):
+                    room.message(str(ret))
+        except:
+            print("weird error")
 
-
-
-rooms = ["hinkiebottesterxd","acleenba"]
+rooms = ["acleenba","hinkiebottesterxd"]
 bot_name = "HinkieBot"
 bot_pw = "fuckthecowboys"
 HinkieBot.easy_start(rooms,bot_name,bot_pw)
